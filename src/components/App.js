b39f4c8940784239ec55534b3bbc958c395e9583
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2017-12-11 12:54:48 -0500

    Set up routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Switch, Route } from 'react-router-dom'
+import Header from './Header'
 import LinkListPage from './LinkListPage'
+import CreateLink from './CreateLink'
 import '../styles/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <LinkListPage />
+      <div className="center w85">
+      	<Header />
+      	<div className='ph3 pv1 background-gray'>
+      		<Switch>
+      			<Route exact path='/' component={LinkListPage} />
+      			<Route exact path='/create' component={CreateLink} />
+      		</Switch>
+      	</div>
       </div>
     );
   }
